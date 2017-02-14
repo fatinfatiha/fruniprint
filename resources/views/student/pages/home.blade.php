cadcc9dc25cb3d60fc8f960a165b78657af4bb04
MAYO\afiq <afiqq18@gmail.com>
2017-02-14 12:24:06 +0800

    small view edit

--- resources/views/student/pages/home.blade.php
+++ resources/views/student/pages/home.blade.php
@@ -30,7 +30,7 @@
       </div>
       <!-- <div class="col-md-12"> -->
       <div class="row">
-        <div class="col-xs-12 col-sm-4 col-sm-push-4 col-md-4 col-md-push-4">
+        <div class="col-xs-12 col-sm-6 col-sm-push-3 col-md-4 col-md-push-4">
 
           <div class="col-xs-2 col-sm-2 col-md-2">
             <div class="animated flipInX card">
